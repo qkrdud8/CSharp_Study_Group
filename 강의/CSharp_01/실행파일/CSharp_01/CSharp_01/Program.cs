83efc57f43237d94a199697cb426aa2a9221c0c6
qkrdud8 <68595923+qkrdud8@users.noreply.github.com>
2020-07-22 19:44:36 +0900

    과제 1

---
+++ 강의/CSharp_01/실행파일/CSharp_01/CSharp_01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharp_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string first, second;
+
+            first = Console.In.ReadLine();
+
+            second = Console.In.ReadLine();
+
+            Console.WriteLine(first+second);
+        }
+    }
+}
